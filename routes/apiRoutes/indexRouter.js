ed7d5f7baee753e04446bad27c920cc78c37ab32
muktar ahmed <muktar.ahmed13@yahoo.com>
2021-12-20 21:43:54 -0600

    added ability to post to json file

--- routes/apiRoutes/indexRouter.js
+++ routes/apiRoutes/indexRouter.js
@@ -2,39 +2,63 @@ const path = require("path");
 const router = require("express").Router();
 const db = require("../../db/db.json");
 const fs = require("fs");
+const uuidv1 = require('uuidv1')
 
 
-function createNewNote( notes) {
-    console.log('notes top' ,notes)
-    fs.writeFileSync(
-      path.join(__dirname, "../../db/db.json"),
-      JSON.stringify( notes, null, 2)
-    );
-notes.push(db)
+// function createNewNote( notes) {
+//     fs.writeFileSync(
+//       path.join(__dirname, "../../db/db.json"),
+//       JSON.stringify( notes, null, 2)
+//     );
+// // notes.push(db)
 
-    console.log('new notes' ,notes)
+//     // notes.id = {id:uuidv1}
+//     // console.log(notes.id = {id:uuidv1()})
+//     // console.log(notes.id)
 
-    // notes.id = {id:uuidv1}
-    // console.log(notes.id = {id:uuidv1()})
-    // console.log(notes.id)
+//     return notes;
 
-    return notes;
-
-  }
+//   }
   
 
   router.get("/notes", (req, res) => {
    res.json(db)
 });
-  router.post("/notes", (req, res) => {
-    createNewNote(req.body)
-    // saveNote(req.body) 
-    // console.log(saveNote)
-    res.status(201).send(req.body);
-    console.log('req' , req.body)
 
+router.delete('/notes', function (req, res) {
+    res.send('DELETE request to homepage')
+  })
+
+router.post("/notes", (req, res) => {
+    console.log("POST request made to /api/notes")
+    console.log(req.body)
+
+    // read-and-save the current contents of db.json
+    fs.readFile(path.join(__dirname, "../../db/db.json"), "utf-8", (err, string)=>{
+      if(err){console.error(err)}
+      var savedArray = JSON.parse(string)
+
+      console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+      console.log("current contents of db.json")
+      console.log(savedArray)
+
+      //add a new note into savedArray
+      // pushing from savedArray into db.json or vice versa?
+savedArray.push(req.body)
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+      //new array into db.json
+      // should i pass savedArray or json in the function?
+      fs.writeFileSync(
+        path.join(__dirname, "../../db/db.json"),
+        JSON.stringify( savedArray, null, 2),
+        savedArray.id = {id:uuidv1()},
+        console.log(savedArray.id)
+      );
+
+      res.status(201).send(savedArray);
+    })
   });
-
 
 
 module.exports = router;
@@ -44,3 +68,12 @@ module.exports = router;
 //     let notes = db
 //     res.json(notes)
 // });
+
+// router.post("/notes", (req, res) => {
+//     createNewNote(req.body)
+//     // saveNote(req.body) 
+//     // console.log(saveNote)
+//     res.status(201).send(req.body);
+//     console.log('req' , req.body)
+
+//   });
